9287ba8efc383f8fc990d5c78f36df4b38a81647
xiaolin <462441355@qq.com>
2018-07-17 20:09:18 +0800

    * 增加错误信息返回

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -12,8 +12,8 @@ class ErrorController extends Yaf_Controller_Abstract
     //从2.1开始, errorAction支持直接通过参数获取异常
     public function errorAction($exception)
     {
-        if ($exception instanceof CodeException){
-            api_response([1,2,3]);
+        if ($exception instanceof CodeException) {
+            api_error($exception->getCode(), $exception->getMessage());
         }
         //1. assign to view engine
         $this->getView()->assign("exception", $exception);
--- application/library/helpers.php
+++ application/library/helpers.php
@@ -1,8 +1,52 @@
 <?php
 
 if (!function_exists('api_response')) {
-
+    /**
+     * @param $data
+     * @param string $code
+     * @param string $msg
+     */
     function api_response($data, $code = '0', $msg = 'ok')
     {
+        $json = [
+            'data' => $data,
+            'code' => $code,
+            'msg' => $msg,
+            'time' => (string)time(),
+            '_ut' => (string)round(microtime(TRUE) - $_SERVER['REQUEST_TIME_FLOAT'], 5),
+        ];
+
+        $response = new Yaf_Response_Http();
+        $response->setHeader('Content-Type', 'application/json;charset=utf-8');
+        $response->setBody(json_encode($json));
+
+        $response->response();
+
+        exit;
+    }
+}
+
+if (!function_exists('api_error')) {
+    /**
+     * @param $data
+     * @param string $code
+     * @param string $msg
+     */
+    function api_error($code = '0', $msg = 'ok', $data = [])
+    {
+        $json = [
+            'data' => $data,
+            'code' => $code,
+            'msg' => $msg,
+            'time' => (string)time(),
+            '_ut' => (string)round(microtime(TRUE) - $_SERVER['REQUEST_TIME_FLOAT'], 5),
+        ];
+
+        $response = new Yaf_Response_Http();
+        $response->setHeader('Content-Type', 'application/json;charset=utf-8');
+        $response->setBody(json_encode($json));
+
+        $response->response();
+        exit;
     }
 }
