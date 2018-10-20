411f648935c67d756d857531344ad3f74f600edf
xiaolin <462441355@qq.com>
2018-10-20 15:58:21 +0800

    * 路由

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -39,6 +39,10 @@ class Bootstrap extends Yaf_Bootstrap_Abstract
     public function _initRoute(Yaf_Dispatcher $dispatcher)
     {
         //在这里注册自己的路由协议,默认使用简单路由
+//        /?m=index&c=index
+//        $route = new Yaf_Route_Simple("m", "c", "a");
+//        $router = Yaf_Dispatcher::getInstance()->getRouter();
+//        $router->addRoute("name", $route);
     }
 
     public function _initView(Yaf_Dispatcher $dispatcher)
