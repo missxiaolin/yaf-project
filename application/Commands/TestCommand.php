0b1f8e691911bf2dce4ee284115d66c5cc97b517
xiaolin <462441355@qq.com>
2018-11-15 07:54:39 +0800

    * 增加脚本单元测试

---
+++ application/Commands/TestCommand.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/11/14
+ * Time: 3:50 PM
+ */
+
+namespace Application\Commands;
+
+use Symfony\Component\Process\Process;
+
+/**
+ * Class TestCommand
+ * @package App\Commands
+ */
+class TestCommand extends Command
+{
+    protected $name = 'test';
+    protected $description = '运行单元测试.';
+
+    /**
+     * TestCommand constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->ignoreValidationErrors();
+    }
+
+    /**
+     * @return int|void
+     */
+    public function handle()
+    {
+        $phpunit = BASE_PATH . '/vendor/bin/phpunit';
+
+        if (!file_exists($phpunit)) {
+            return $this->error('请先安装依赖：composer install -vvv');
+        }
+
+        $process = new Process("composer dump && $phpunit -d memory_limit=1024M " . $this->getCommandLine(true));
+
+        return $process->setTimeout(360)->run(function ($type, $buffer) {
+            $this->output->write($buffer);
+        });
+    }
+}
